d20b004a9884812daec3096e1cb742a3843600d5
smz7z <santimgb98@gmail.com>
2025-09-20 22:59:45 +0200

    Agregar a github

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button button_back = findViewById(R.id.button_back);
+        TextView textView = findViewById(R.id.textView);
+        EditText input = findViewById(R.id.input);
+        String texto = input.getText().toString();
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if(texto.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Por favor ingresa tu nombre", Toast.LENGTH_SHORT).show();
+                    return;
+                } else{
+                    button_back.setVisibility(View.VISIBLE);
+                    input.setVisibility(View.GONE);
+                    button.setVisibility(View.GONE);
+                    textView.setVisibility(View.VISIBLE);
+                    textView.setText("Hola"+ texto);
+                }
+
+            }
+        });
+
+        button_back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                button_back.setVisibility(View.GONE);
+                input.setText("");
+                input.setVisibility(View.VISIBLE);
+                button.setVisibility(View.VISIBLE);
+                textView.setVisibility(View.GONE);
+            }
+        });
+    }
+}
